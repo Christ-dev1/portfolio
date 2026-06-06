0d26ee4fe4fbfb8d9b88bf7206d6334439aa4d24
Christ-dev1 <christ.kadjo@epitech.eu>
2026-06-06 23:29:31 +0000

    fix: add vercel URLs to CORS

--- portfolio-backend/config/cors.php
+++ portfolio-backend/config/cors.php
@@ -5,7 +5,9 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:3000',
+        'https://portfolio-rc8s73xbb-kadjoblin3-2694s-projects.vercel.app',
         'https://portfolio-azure-seven-73.vercel.app',
+        'https://*.vercel.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
