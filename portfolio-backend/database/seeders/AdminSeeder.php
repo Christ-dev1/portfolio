65bd8ca8eed4590ad9c43ca9b7eb09fd61caee8b
Christ-dev1 <christ.kadjo@epitech.eu>
2026-06-06 22:18:41 +0000

    Update AdminSeeder.php

--- portfolio-backend/database/seeders/AdminSeeder.php
+++ portfolio-backend/database/seeders/AdminSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class AdminSeeder extends Seeder
@@ -10,8 +12,18 @@ class AdminSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        //
-    }
+   public function run()
+{
+    DB::table('users')->updateOrInsert(
+        ['email' => 'admin@example.com'],
+        [
+            'name' => 'Admin',
+            'password' => '$2y$12$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]
+    );
+}
+
+
 }
