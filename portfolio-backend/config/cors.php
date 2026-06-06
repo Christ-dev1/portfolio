a5885259a483b195e42433898e798179fe1667d6
Christ-dev1 <christ.kadjo@epitech.eu>
2026-06-06 20:27:28 +0000

    Fix CORS for Vercel frontend

--- portfolio-backend/config/cors.php
+++ portfolio-backend/config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:3000',
-        'http://127.0.0.1:3000',
+        'https://portfolio-azure-seven-73.vercel.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
