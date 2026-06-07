ca3853e6ee698a8601f5e544e778dec64bb88c3b
Christ-dev1 <christ.kadjo@epitech.eu>
2026-06-07 18:02:33 +0000

    Update create.blade.php

--- portfolio-backend/resources/views/admin/formations/create.blade.php
+++ portfolio-backend/resources/views/admin/formations/create.blade.php
@@ -3,9 +3,12 @@
 @section('content')
 <div class="container">
     <h1 class="mb-4">Ajouter une formation</h1>
+
     <form action="{{ route('admin.formations.store') }}" method="POST">
         @csrf
+
         @include('admin.formations.form')
+
         <button type="submit" class="btn btn-primary">Enregistrer</button>
         <a href="{{ route('admin.formations.index') }}" class="btn btn-secondary">Annuler</a>
     </form>
