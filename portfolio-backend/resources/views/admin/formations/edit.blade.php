bd17820e6e2932edbc88122c4636fc3f79269a29
Christ-dev1 <christ.kadjo@epitech.eu>
2026-06-07 18:02:17 +0000

    Update edit.blade.php

--- portfolio-backend/resources/views/admin/formations/edit.blade.php
+++ portfolio-backend/resources/views/admin/formations/edit.blade.php
@@ -3,9 +3,13 @@
 @section('content')
 <div class="container">
     <h1 class="mb-4">Modifier la formation</h1>
+
     <form action="{{ route('admin.formations.update', $formation) }}" method="POST">
-        @csrf @method('PUT')
+        @csrf
+        @method('PUT')
+
         @include('admin.formations.form')
+
         <button type="submit" class="btn btn-warning">Mettre à jour</button>
         <a href="{{ route('admin.formations.index') }}" class="btn btn-secondary">Annuler</a>
     </form>
