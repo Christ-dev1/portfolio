91d048b060bfc0475b0b0ad81e3e77efb6053a99
Christ-dev1 <christ.kadjo@epitech.eu>
2026-06-07 18:07:16 +0000

    Update create.blade.php

--- portfolio-backend/resources/views/admin/projects/create.blade.php
+++ portfolio-backend/resources/views/admin/projects/create.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.admin', ['title' => 'Ajouter un projet'])
 
 @section('content')
-    <div class="card">
-        <h1>Ajouter un projet</h1>
-        <form method="POST" action="{{ route('admin.projects.store') }}" enctype="multipart/form-data">
+    <div class="card" style="
+        max-width: 900px;
+        width: 100%;
+        margin: 0 auto;
+        padding: 16px;
+        box-sizing: border-box;
+    ">
+        <h1 style="margin-bottom: 16px;">Ajouter un projet</h1>
+
+        <form method="POST"
+              action="{{ route('admin.projects.store') }}"
+              enctype="multipart/form-data">
+
+            @csrf
+
             @include('admin.projects.form', ['buttonText' => 'Ajouter'])
+
         </form>
     </div>
 @endsection
