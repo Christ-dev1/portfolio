93de41307ce0e3f1866aff9d6925483181ae1599
Christ-dev1 <christ.kadjo@epitech.eu>
2026-06-07 18:07:39 +0000

    Update edit.blade.php

--- portfolio-backend/resources/views/admin/projects/edit.blade.php
+++ portfolio-backend/resources/views/admin/projects/edit.blade.php
@@ -1,11 +1,25 @@
 @extends('layouts.admin', ['title' => 'Modifier un projet'])
 
 @section('content')
-    <div class="card">
-        <h1>Modifier un projet</h1>
-        <form method="POST" action="{{ route('admin.projects.update', $project) }}" enctype="multipart/form-data">
+    <div class="card" style="
+        max-width: 100%;
+        width: 100%;
+        max-width: 900px;
+        margin: 0 auto;
+        padding: 16px;
+        box-sizing: border-box;
+    ">
+        <h1 style="margin-bottom: 16px;">Modifier un projet</h1>
+
+        <form method="POST"
+              action="{{ route('admin.projects.update', $project) }}"
+              enctype="multipart/form-data">
+
+            @csrf
             @method('PUT')
+
             @include('admin.projects.form', ['buttonText' => 'Modifier'])
+
         </form>
     </div>
 @endsection
